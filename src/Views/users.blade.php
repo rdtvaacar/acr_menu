56e322b81358abd6a6ef9ed44f9e1d5e71f13fd5
acr <acarbey15@gmail.com>
2017-06-10 14:08:57 +0300

    yaz

--- src/Views/users.blade.php
+++ src/Views/users.blade.php
@@ -3,54 +3,59 @@
     <link rel="stylesheet" href="/plugins/datatables/dataTables.bootstrap.css">
 @stop
 @section('acr_menu')
-    <div class="box">
-        <div class="box-header">
-            <h4 style="float:left;">Tüm Üyeler</h4>
-            <form class="search-form" style="float: right; width: 200px;" method="POST" action="/acr/menu/users/search">
-                {{csrf_field()}}
-                <div class="input-group">
-                    <input type="text" name="search" class="form-control" placeholder="Search">
-                    <div class="input-group-btn">
-                        <button type="submit" name="submit" class="btn btn-danger btn-flat"><i class="fa fa-search"></i>
-                        </button>
-                    </div>
-                </div>
-                <!-- /.input-group -->
-            </form>
-        </div>
-
-        <!-- /.box-header -->
-        <div class="box-body">
-            <table class="table table-hover">
-                <tr>
-                    <th>User</th>
-                    <th>Email</th>
-                    <th>Role</th>
-                </tr>
-                @foreach($users as $user)
-                    <tr>
-                        <td>{{$user->name}}</td>
-                        <td>{{$user->email}}</td>
-                        <td>
-                            <div class="btn-group" data-toggle="buttons">
-
-                                @foreach($roles as $role)
-                                    <label class="btn btn-primary {{in_array($role->id, $role_ids[$user->id])? 'active':''}} ">
-                                        <input class="user_role_input" {{in_array($role->id, $role_ids[$user->id])? 'checked':''}} autocomplete="off" value="{{$user->id.'_'.$role->id}}" type="checkbox"/>
-                                        {{$role->name}}
-                                    </label>
-                                @endforeach
+    <section class="content">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="box box-primary">
+                    <div class="box-header">
+                        <h4 style="float:left;">Tüm Üyeler</h4>
+                        <form class="search-form" style="float: right; width: 200px;" method="POST" action="/acr/menu/users/search">
+                            {{csrf_field()}}
+                            <div class="input-group">
+                                <input type="text" name="search" class="form-control" placeholder="Search">
+                                <div class="input-group-btn">
+                                    <button type="submit" name="submit" class="btn btn-danger btn-flat"><i class="fa fa-search"></i>
+                                    </button>
+                                </div>
                             </div>
-                        </td>
-                    </tr>
-                @endforeach
+                            <!-- /.input-group -->
+                        </form>
+                    </div>
 
-            </table>
-            {{$users->appends(['search'=>$s])->links()}}
+                    <!-- /.box-header -->
+                    <div class="box-body">
+                        <table class="table table-hover">
+                            <tr>
+                                <th>User</th>
+                                <th>Email</th>
+                                <th>Role</th>
+                            </tr>
+                            @foreach($users as $user)
+                                <tr>
+                                    <td>{{$user->name}}</td>
+                                    <td>{{$user->email}}</td>
+                                    <td>
+                                        <div class="btn-group" data-toggle="buttons">
+
+                                            @foreach($roles as $role)
+                                                <label class="btn btn-primary {{in_array($role->id, $role_ids[$user->id])? 'active':''}} ">
+                                                    <input class="user_role_input" {{in_array($role->id, $role_ids[$user->id])? 'checked':''}} autocomplete="off" value="{{$user->id.'_'.$role->id}}" type="checkbox"/>
+                                                    {{$role->name}}
+                                                </label>
+                                            @endforeach
+                                        </div>
+                                    </td>
+                                </tr>
+                            @endforeach
+
+                        </table>
+                        {{$users->appends(['search'=>$s])->links()}}
+                    </div>
+
+                </div>
+            </div>
         </div>
-
-    </div>
-
+    </section>
 @stop
 @section('footer')
     <script>
